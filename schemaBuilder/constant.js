b41f590064bcd2f5006ff79cffd2dec656593163
sandeep-sana <sandeep.s@clay.co.in>
2025-09-19 12:34:58 +0530

    client

--- schemaBuilder/constant.js
+++ schemaBuilder/constant.js
@@ -1,7 +1,19 @@
 export const DATA_TYPE = [
-    {label: "String", value: "STRING"},
-    {label: "Boolean", value: "BOOLEAN"},
-    {label: "Number", value: "NUMBER"},
-    {label: "Date", value: "DATE"},
-    {label: "Array", value: "ARRAY"},
+    { label: "String", value: "STRING" },
+    { label: "Boolean", value: "BOOLEAN" },
+    { label: "Number", value: "NUMBER" },
+    { label: "Date", value: "DATE" },
+    { label: "Array", value: "ARRAY" },
+]
+
+export const ADD_COLUMN = {
+    columnName: 'columnName',
+    type: 'STRING',
+    default: null,
+    enum: [],
+}
+
+export const BOOLEAN = [
+    { label: "True", value: true },
+    { label: "False", value: false },
 ]
