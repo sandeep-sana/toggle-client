bfb1c86e13ba18a5f3bbe8ed6553f7105fb38bee
sandeep-sana <sandeep.s@clay.co.in>
2025-12-30 14:03:04 +0530

    login

---
+++ app/pages/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Form @submit="submit">
+      <!-- Email -->
+      <div>
+        <label for="email">Email</label>
+        <Field name="email" placeholder="Email" rules="required|nospace|email" v-model="login.email"></Field>
+        <ErrorMessage name="email"></ErrorMessage>
+      </div>
+      <button type="submit">Submit</button>
+    </Form>
+  </div>
+</template>
+
+<script setup>
+import { Field, ErrorMessage, Form } from 'vee-validate';
+
+// variables
+const login = reactive({});
+
+// methods
+const submit = async() => {
+  
+}
+
+
+
+</script>
+
+<style scoped></style>
